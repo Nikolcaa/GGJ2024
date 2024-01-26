b9411b5e09ab662becf9afdcfcb44faba7742127
Nikola Marjanović <nikolals05@outlook.com>
2024-01-26 15:02:53 +0100

    TEst enemy created

---
+++ Assets/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("PROPERTIES")]
+    [SerializeField] private float enemySpeed;
+    [SerializeField] private float enemyHp;
+    [SerializeField] private float enemyDamage;
+
+}
